338540179453e7f5bf2ab8ddbd6612af9c6daf77
amrwael325 <168336811+amrwael325@users.noreply.github.com>
2025-11-04 14:38:49 +0200

    Add files via upload

---
+++ ASIC/Formality/post-dft/dft_fm_script.tcl
@@ -0,0 +1,122 @@
+
+########################### Define Top Module ############################
+                                                   
+set top_module SYS_TOP
+
+######################### Formality Setup File ###########################
+
+set synopsys_auto_setup true
+
+set_svf "../../DFT/$top_module.svf"
+
+
+set SSLIB "/home/IC/tsmc_fb_cl013g_sc/aci/sc-m/synopsys/scmetro_tsmc_cl013g_rvt_ss_1p08v_125c.db"
+set TTLIB "/home/IC/tsmc_fb_cl013g_sc/aci/sc-m/synopsys/scmetro_tsmc_cl013g_rvt_tt_1p2v_25c.db"
+set FFLIB "/home/IC/tsmc_fb_cl013g_sc/aci/sc-m/synopsys/scmetro_tsmc_cl013g_rvt_ff_1p32v_m40c.db"
+
+######################### Reference Container ############################
+
+## Read Reference technology libraries
+
+read_db -container r $SSLIB
+read_db -container r $TTLIB
+read_db -container r $FFLIB
+
+## Read Reference Design Files
+
+read_verilog -container r "/home/IC/Projects/System/RTL/SYS_TOP_dft.v"
+read_verilog -container r "/home/IC/Projects/System/RTL/ALU_16B.v"
+read_verilog -container r "/home/IC/Projects/System/RTL/clkdiv.v"
+read_verilog -container r "/home/IC/Projects/System/RTL/CLKDIV_MUX.v"
+read_verilog -container r "/home/IC/Projects/System/RTL/CLK_GATE.v"
+read_verilog -container r "/home/IC/Projects/System/RTL/DATA_SYNC.v"
+read_verilog -container r "/home/IC/Projects/System/RTL/Register_file.v"
+read_verilog -container r "/home/IC/Projects/System/RTL/RST_SYNC.v"
+read_verilog -container r "/home/IC/Projects/System/RTL/FIFO_MEM_CNTRL.v"
+read_verilog -container r "/home/IC/Projects/System/RTL/DF_SYNC.v"
+read_verilog -container r "/home/IC/Projects/System/RTL/FIFO_RD.v"
+read_verilog -container r "/home/IC/Projects/System/RTL/FIFO_WR.v"
+read_verilog -container r "/home/IC/Projects/System/RTL/ASYNC_FIFO.v"
+read_verilog -container r "/home/IC/Projects/System/RTL/PULSE_GEN.v"
+read_verilog -container r "/home/IC/Projects/System/RTL/data_sampling.v"
+read_verilog -container r "/home/IC/Projects/System/RTL/deserializer.v"
+read_verilog -container r "/home/IC/Projects/System/RTL/edge_bit_counter.v"
+read_verilog -container r "/home/IC/Projects/System/RTL/parity_check.v"
+read_verilog -container r "/home/IC/Projects/System/RTL/stop_check.v"
+read_verilog -container r "/home/IC/Projects/System/RTL/start_check.v"
+read_verilog -container r "/home/IC/Projects/System/RTL/UART_RX.v"
+read_verilog -container r "/home/IC/Projects/System/RTL/FSM.v"
+read_verilog -container r "/home/IC/Projects/System/RTL/MUX.v"
+read_verilog -container r "/home/IC/Projects/System/RTL/mux2X1.v"
+read_verilog -container r "/home/IC/Projects/System/RTL/Parity_Calc.v"
+read_verilog -container r "/home/IC/Projects/System/RTL/serializer.v"
+read_verilog -container r "/home/IC/Projects/System/RTL/UART_tx.v"
+read_verilog -container r "/home/IC/Projects/System/RTL/FSM_tx.v"
+read_verilog -container r "/home/IC/Projects/System/RTL/UART.v"
+read_verilog -container r "/home/IC/Projects/System/RTL/SYS_CTRL.v"
+
+## set the top Reference Design 
+
+set_top $top_module
+
+######################## Implementation Container #########################
+
+## Read Implementation technology libraries
+
+read_db -container i $SSLIB
+read_db -container i $TTLIB
+read_db -container i $FFLIB
+
+## Read Implementation Design Files
+
+read_verilog -netlist -container i "/home/IC/Projects/System/Backend/DFT/netlists/SYS_TOP.v"
+ 
+## set the top Implementation Design
+
+set_top $top_module
+
+############################### Don't verify #################################
+
+# do not verify scan in & scan out ports as a compare point as it is existed only after synthesis and not existed in the RTL
+
+#scan in
+
+set_dont_verify_points -type port r:/WORK/*/SI*
+set_dont_verify_points -type port i:/WORK/*/SI*
+
+#scan_out
+set_dont_verify_points -type port r:/WORK/*/SO*
+set_dont_verify_points -type port i:/WORK/*/SO*
+
+
+############################### constants #####################################
+
+# all atpg enable(test_mode, scan_enable) are zero during formal compare
+
+#test_mode
+set_constant r:/WORK/*/test_mode 0
+set_constant i:/WORK/*/test_mode 0
+
+#scan_enable
+set_constant r:/WORK/*/SE 0
+set_constant i:/WORK/*/SE 0
+
+########################### matching Compare points ##########################
+
+match
+
+################################# verify #####################################
+
+set successful [verify]
+if {!$successful} {
+diagnose
+analyze_points -failing
+}
+
+report_passing_points > "reports/passing_points.rpt"
+report_failing_points > "reports/failing_points.rpt"
+report_aborted_points > "reports/aborted_points.rpt"
+report_unverified_points > "reports/unverified_points.rpt"
+
+
+start_gui
